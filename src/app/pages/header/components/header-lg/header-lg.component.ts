43eae4a66ccef9fce0bf2f01247d0709d666170d
deduc <deductivegoose@outlook.es>
2023-10-26 21:52:41 +0200

    bootstrap y angular material añadidos. Muchas modificaciones hechas en headerComponent

---
+++ src/app/pages/header/components/header-lg/header-lg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pages-header-lg',
+  templateUrl: './header-lg.component.html',
+  styleUrls: ['./header-lg.component.css']
+})
+export class HeaderLgComponent {
+
+}
---
+++ src/app/pages/header/components/header-sm-md/header-sm-md.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pages-header-sm-md',
+  templateUrl: './header-sm-md.component.html',
+  styleUrls: ['./header-sm-md.component.css']
+})
+export class HeaderSmMdComponent {
+
+}
